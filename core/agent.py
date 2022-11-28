eb8bf0f87db8a580132b1a09562144926d6442a3
Joseph Bunao <jbunao@aim.edu>
2022-11-28 12:10:08 -0500

    backward actions

--- core/agent.py
+++ core/agent.py
@@ -1,13 +1,79 @@
 import tensorflow as tf
+from core.policy_network import PolicyNetwork
 
 class Agent:
-    def __init__(self, spin_j):
-        self.spin_j = spin_j
-        self.intertwiner_dim = int(2*spin_j + 1)
+    NEG_INF = -10000000000.0
 
-    def action(self, state):
-        pass
+    def __init__(self, env_grid_dim, env_grid_length):
+        self.env_grid_dim = env_grid_dim
+        self.env_grid_length = env_grid_length
+        self.backward_action_dim = self.env_grid_dim
+        self.forward_action_dim = self.env_grid_dim + 1
 
+        self.policy = PolicyNetwork(
+            main_layer_nodes=[15],
+            branch1_layer_nodes=[self.backward_action_dim],
+            branch2_layer_nodes=[self.forward_action_dim],
+        )
+
+    def act_backwards(self, current_position):
+        encoded_position = self._encode_position(current_position)
+        backward_action_logits = self.policy.predict(encoded_position)[0]
+
+        action_mask = self._find_forbidden_backward_actions(encoded_position)
+        masked_logits = self._mask_action_logits(
+            backward_action_logits, action_mask
+        )
+
+        action_indices = self._choose_action(masked_logits)
+        encoded_actions = self._encode_backward_action(action_indices)
+
+        is_still_sampling = self._check_if_able_to_act_backwards(action_mask)
+        backward_actions = encoded_actions*is_still_sampling
+        return backward_actions
+
+    @staticmethod
+    def _choose_action(logits):
+        action_indices = tf.random.categorical(logits, 1)
+        return action_indices
+
+    def _encode_position(self, position):
+        encoded_position = tf.one_hot(
+            position, depth=self.env_grid_length, axis=-1
+        )
+        return encoded_position
+
+    def _encode_backward_action(self, action_indices):
+        encoded_actions = tf.one_hot(
+            tf.reshape(action_indices, shape=(-1,)),
+            depth=self.backward_action_dim,
+            dtype=tf.int32
+        )
+        return encoded_actions
+
+    @staticmethod
+    def _find_forbidden_backward_actions(position):
+        backwards_action_mask = tf.math.equal(position, 0)
+        return backwards_action_mask
+
+    def _check_if_able_to_act_backwards(self, backwards_action_mask):
+        is_at_origin = tf.math.reduce_all(
+            backwards_action_mask, axis=1, keepdims=True
+        )
+        is_able = tf.cast(
+            tf.math.logical_not(is_at_origin),
+            dtype=tf.int32
+        )
+        return is_able
+
+    def _mask_action_logits(self, action_logits, mask):
+        avoid_inds = tf.where(mask)
+        # Need validation that masked are not sampled
+        masked_logits = tf.tensor_scatter_nd_add(
+            action_logits, avoid_inds,
+            tf.constant([self.NEG_INF] * avoid_inds.shape[0])
+        )
+        return masked_logits
 
 def _encode_state(state, intertwiner_dim):
     encoded = tf.reshape(
--- core/policy_network.py
+++ core/policy_network.py
@@ -2,25 +2,41 @@ import tensorflow as tf
 
 
 class PolicyNetwork(tf.keras.Model):
-    def __init__(self, n_input, n_hidden, n_output):
+    def __init__(
+            self, main_layer_nodes, branch1_layer_nodes, branch2_layer_nodes
+    ):
         super().__init__()
-        self.n_input = n_input
-        self.n_hidden = n_hidden
-        self.n_output = n_output
-        self.layer_1 = tf.keras.layers.Dense(self.n_hidden)
-        self.layer_1.build(self.n_input)
-        self.layer_2 = tf.keras.layers.Dense(self.n_hidden)
-        self.layer_2.build(self.n_hidden)
+        self.flatten = tf.keras.layers.Flatten()
+        self.main_layers = [
+            tf.keras.layers.Dense(n_nodes) for n_nodes in main_layer_nodes
+        ]
+        self.branch1_layers = [
+            tf.keras.layers.Dense(n_nodes) for n_nodes in branch1_layer_nodes
+        ]
+        self.branch2_layers = [
+            tf.keras.layers.Dense(n_nodes) for n_nodes in branch2_layer_nodes
+        ]
+        # TODO: build layers
+        # self.layer_1.build(self.n_input)
 
-        self.output_layer_1 = tf.keras.layers.Dense(self.n_output)
-        self.output_layer_1.build(self.n_hidden)
-        self.output_layer_2 = tf.keras.layers.Dense(self.n_output)
-        self.output_layer_2.build(self.n_hidden)
 
-    def call(self, input):
-        hidden = self.layer_1(input)
-        hidden = tf.keras.activations.tanh(hidden)
-        hidden = self.layer_2(hidden)
-        hidden = tf.keras.activations.tanh(hidden)
+    def call(self, data_input):
+        hidden = self.flatten(data_input)
+        for main_layer in self.main_layers:
+            hidden = main_layer(hidden)
+            hidden = tf.keras.activations.tanh(hidden)
 
+        b1_hidden = hidden
+        for b1_layer in self.branch1_layers[:-1]:
+            b1_hidden = b1_layer(b1_hidden)
+            b1_hidden = tf.keras.activations.tanh(b1_hidden)
+
+        b2_hidden = hidden
+        for b2_layer in self.branch2_layers[:-1]:
+            b2_hidden = b2_layer(b2_hidden)
+            b2_hidden = tf.keras.activations.tanh(b2_hidden)
+
+        output1 = self.branch1_layers[-1](b1_hidden)
+        output2 = self.branch2_layers[-1](b2_hidden)
+        return output1, output2
         
