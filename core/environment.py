a60eea64711745d64e8579ef57f9681af3d47264
Joseph Bunao <jbunao@aim.edu>
2022-12-01 18:15:39 -0500

    start reward function

--- core/environment.py
+++ core/environment.py
@@ -29,6 +29,9 @@ class HypergridEnvironment:
     def __init__(self, grid_dimension, grid_length):
         self.grid_dimension = grid_dimension
         self.grid_length = grid_length
+        self.rewards = _generate_grid_rewards(
+            grid_dimension, grid_length, 0.001, 1, 1
+        )
 
     @tf.function
     def reset_for_backward_sampling(self, batch_size):
@@ -57,3 +60,32 @@ class HypergridEnvironment:
     def step_forward(current_position, forward_action):
         new_position = current_position + forward_action[:, :-1]
         return new_position
+
+    @tf.function
+    def get_rewards(self, positions):
+        rewards = tf.reshape(tf.gather_nd(self.rewards, positions), shape=(-1, 1))
+        return rewards
+
+
+def _generate_grid_rewards(grid_dimension, grid_length, r0, r1, r2):
+    # grid_length >= 7
+    coords_transf = tf.math.abs(tf.range(grid_length, dtype=tf.float32) / (grid_length - 1) - 0.5)
+
+    mid_level_1d = tf.cast(tf.math.greater(coords_transf, 0.25), dtype=tf.float32)
+    high_level_1d = tf.cast(
+        tf.math.logical_and(
+            tf.math.greater(coords_transf, 0.3), tf.math.less(coords_transf, 0.4)),
+        dtype=tf.float32
+    )
+
+    mid_level = tf.reshape(mid_level_1d, shape=[-1] + [1] * (grid_dimension - 1))
+    high_level = tf.reshape(high_level_1d, shape=[-1] + [1] * (grid_dimension - 1))
+
+    for i in range(1, grid_dimension):
+        shape = [1] * grid_dimension
+        shape[i] = -1
+        mid_level *= tf.reshape(mid_level_1d, shape=shape)
+        high_level *= tf.reshape(high_level_1d, shape=shape)
+
+    rewards = r0 + r1 * mid_level + r2 * high_level
+    return rewards
