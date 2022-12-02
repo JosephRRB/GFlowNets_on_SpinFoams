0e5255f127f3326f91b294fcb6d7d06a973c46a4
Joseph Bunao <jbunao@aim.edu>
2022-12-02 14:43:05 -0500

    training trials

--- core/runner.py
+++ core/runner.py
@@ -5,15 +5,57 @@ from core.agent import Agent
 
 
 class Runner:
-    def __init__(self, grid_dimension, grid_length):
+    def __init__(self, grid_dimension, grid_length, learning_rate=0.0005):
         self.agent = Agent(
             env_grid_dim=grid_dimension, env_grid_length=grid_length
         )
         self.env = HypergridEnvironment(
             grid_dimension=grid_dimension, grid_length=grid_length
         )
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-    def generate_backward_trajectories(self, batch_size):
+    def train_agent(self, batch_size, n_iterations):
+        ave_losses = []
+        for i in range(n_iterations):
+            ave_loss = self._training_step(batch_size)
+            ave_losses.append(ave_loss)
+
+            if i % 100 == 0:
+                print(f"Iteration: {i}, Average Loss: {float(ave_loss):.5f}")
+        ave_losses = tf.concat(ave_losses, axis=0)
+        return ave_losses
+
+    def _training_step(self, batch_size):
+        ts1, ba1, fa1 = self._generate_backward_trajectories(batch_size)
+        ts2, ba2, fa2 = self._generate_forward_trajectories(batch_size, training=True)
+
+        final_positions = tf.concat([ts1[0], ts2[-1]], axis=0)
+        rewards = self.env.get_rewards(final_positions)
+        with tf.GradientTape() as tape:
+            alpr1 = self.agent.calculate_action_log_probability_ratio(ts1, ba1, fa1)
+            alpr2 = self.agent.calculate_action_log_probability_ratio(ts2, ba2, fa2)
+            action_log_proba_ratios = tf.concat([alpr1, alpr2], axis=0)
+            log_Z0 = self.agent.log_Z0
+            log_rewards = tf.math.log(rewards)
+            ave_loss = self._calculate_ave_loss(log_Z0, log_rewards, action_log_proba_ratios)
+
+        grads = tape.gradient(
+            ave_loss, self.agent.policy.trainable_weights + [self.agent.log_Z0]
+        )
+        self.optimizer.apply_gradients(
+            zip(grads, self.agent.policy.trainable_weights + [self.agent.log_Z0])
+        )
+        return ave_loss
+
+
+    @staticmethod
+    @tf.function
+    def _calculate_ave_loss(log_Z0, log_rewards, action_log_proba_ratios):
+        loss = tf.math.square(log_Z0 - log_rewards + action_log_proba_ratios)
+        ave_loss = tf.reduce_mean(loss)
+        return ave_loss
+
+    def _generate_backward_trajectories(self, batch_size):
         current_position = self.env.reset_for_backward_sampling(batch_size)
 
         no_coord_action = tf.zeros(
@@ -49,7 +91,7 @@ class Runner:
         forward_actions = tf.concat([tf.stack(forward_actions), stop_actions], axis=2)
         return trajectories, backward_actions, forward_actions
 
-    def generate_forward_trajectories(self, batch_size):
+    def _generate_forward_trajectories(self, batch_size, training=False):
         current_position = self.env.reset_for_forward_sampling(batch_size)
         is_still_sampling = tf.ones(
             shape=(batch_size, 1), dtype=tf.int32
@@ -65,7 +107,7 @@ class Runner:
         at_least_one_ongoing = tf.constant(True)
         while at_least_one_ongoing:
             action, is_still_sampling = self.agent.act_forward(
-                current_position, is_still_sampling
+                current_position, is_still_sampling, training=training
             )
             current_position = self.env.step_forward(current_position, action)
 
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -9,7 +9,7 @@ def test_actions_correctly_correspond_to_backward_trajectories():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    trajectories, backward_actions, forward_actions = runner.generate_backward_trajectories(batch_size)
+    trajectories, backward_actions, forward_actions = runner._generate_backward_trajectories(batch_size)
 
     next_backward_positions = trajectories[:-1] - backward_actions[:-1]
     next_forward_positions = trajectories[1:] + forward_actions[1:, :, :-1]
@@ -24,7 +24,7 @@ def test_backward_trajectories_do_not_go_out_of_bounds():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    trajectories, _, _ = runner.generate_backward_trajectories(batch_size)
+    trajectories, _, _ = runner._generate_backward_trajectories(batch_size)
 
     tf.debugging.assert_greater_equal(trajectories, 0)
 
@@ -35,7 +35,7 @@ def test_last_positions_for_backward_trajectories_are_all_0():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    trajectories, _, _ = runner.generate_backward_trajectories(batch_size)
+    trajectories, _, _ = runner._generate_backward_trajectories(batch_size)
 
     tf.debugging.assert_equal(trajectories[-1], 0)
 
@@ -46,7 +46,7 @@ def test_first_forward_actions_for_backward_trajectories_are_all_stop_actions():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    _, _, forward_actions = runner.generate_backward_trajectories(batch_size)
+    _, _, forward_actions = runner._generate_backward_trajectories(batch_size)
 
     tf.debugging.assert_equal(forward_actions[0, :, :-1], 0)
     tf.debugging.assert_equal(forward_actions[0, :, -1], 1)
@@ -58,7 +58,7 @@ def test_last_backward_actions_for_backward_trajectories_are_all_0():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    _, backward_actions, _ = runner.generate_backward_trajectories(batch_size)
+    _, backward_actions, _ = runner._generate_backward_trajectories(batch_size)
 
     tf.debugging.assert_equal(backward_actions[-1], 0)
 
@@ -69,7 +69,7 @@ def test_actions_correctly_correspond_to_forward_trajectories():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    trajectories, backward_actions, forward_actions = runner.generate_forward_trajectories(batch_size)
+    trajectories, backward_actions, forward_actions = runner._generate_forward_trajectories(batch_size)
 
     next_forward_positions = trajectories[:-1] + forward_actions[:-1, :, :-1]
     next_backward_positions = trajectories[1:] - backward_actions[1:]
@@ -84,7 +84,7 @@ def test_forward_trajectories_do_not_go_out_of_bounds():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    trajectories, _, _ = runner.generate_forward_trajectories(batch_size)
+    trajectories, _, _ = runner._generate_forward_trajectories(batch_size)
 
     tf.debugging.assert_less_equal(trajectories, grid_length-1)
 
@@ -95,7 +95,7 @@ def test_first_positions_for_forward_trajectories_are_all_0():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    trajectories, _, _ = runner.generate_forward_trajectories(batch_size)
+    trajectories, _, _ = runner._generate_forward_trajectories(batch_size)
 
     tf.debugging.assert_equal(trajectories[0], 0)
 
@@ -106,7 +106,7 @@ def test_stop_action_correctly_stops_forward_trajectories():
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    trajectories, _, forward_actions = runner.generate_forward_trajectories(batch_size)
+    trajectories, _, forward_actions = runner._generate_forward_trajectories(batch_size)
 
     stop_actions = forward_actions[:, :, -1]
     stop_action_indices = tf.where(stop_actions)
@@ -118,12 +118,13 @@ def test_stop_action_correctly_stops_forward_trajectories():
     tf.debugging.assert_equal(coord_when_stop_was_chosen, corresponding_last_coords)
     tf.debugging.assert_equal(tf.reduce_sum(stop_actions, axis=0), 1)
 
+
 def test_first_backward_actions_for_forward_trajectories_are_all_0():
     grid_dim = 5
     grid_length = 8
     batch_size = 10
 
     runner = Runner(grid_dimension=grid_dim, grid_length=grid_length)
-    _, backward_actions, _ = runner.generate_forward_trajectories(batch_size)
+    _, backward_actions, _ = runner._generate_forward_trajectories(batch_size)
 
     tf.debugging.assert_equal(backward_actions[0], 0)
