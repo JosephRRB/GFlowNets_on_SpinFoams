ff906dcd7663c70e9ff9ede16b1f07c41e23775f
Joseph Bunao <jrrbunao721@gmail.com>
2023-05-02 22:30:44 -0400

    star model

--- core/environment.py
+++ core/environment.py
@@ -6,6 +6,58 @@ import tensorflow as tf
 ROOT_DIR = os.path.abspath(__file__ + "/../../")
 
 
+class BaseSpinFoam:
+    def __init__(self, n_boundary_intertwiners):
+        self.n_boundary_intertwiners = n_boundary_intertwiners
+
+    @tf.function
+    def get_spinfoam_amplitudes(self, amplitudes, boundary_intertwiners):
+        pass
+
+
+class StarModelSpinFoam(BaseSpinFoam):
+    def __init__(self):
+        super().__init__(n_boundary_intertwiners=20)
+
+    @staticmethod
+    @tf.function
+    def _get_amplitude_per_star_edge(amplitudes, boundary_intertwiners_per_edge):
+        amplitude = tf.gather_nd(amplitudes, boundary_intertwiners_per_edge)
+        return amplitude
+
+    @tf.function
+    def get_spinfoam_amplitudes(self, amplitudes, boundary_intertwiners):
+        vertex_1 = self._get_amplitude_per_star_edge(
+            amplitudes, boundary_intertwiners[:, :4]
+        )
+        vertex_2 = self._get_amplitude_per_star_edge(
+            amplitudes, boundary_intertwiners[:, 4:8]
+        )
+        vertex_3 = self._get_amplitude_per_star_edge(
+            amplitudes, boundary_intertwiners[:, 8:12]
+        )
+        vertex_4 = self._get_amplitude_per_star_edge(
+            amplitudes, boundary_intertwiners[:, 12:16]
+        )
+        vertex_5 = self._get_amplitude_per_star_edge(
+            amplitudes, boundary_intertwiners[:, 16:20]
+        )
+
+        star_amplitudes = tf.einsum(
+            "abcde, ie, id, ic, ib, ia -> i",
+            amplitudes, vertex_1, vertex_2, vertex_3, vertex_4, vertex_5
+        )
+        return star_amplitudes
+
+
+class SingleVertexSpinFoam(BaseSpinFoam):
+    def __init__(self):
+        super().__init__(n_boundary_intertwiners=5)
+
+    def get_spinfoam_amplitudes(self, amplitudes, boundary_intertwiners):
+        return tf.gather_nd(amplitudes, boundary_intertwiners)
+
+
 class SpinFoamEnvironment:
     """
     This class creates the hypergrid environment which the GFlowNet agent
@@ -21,15 +73,18 @@ class SpinFoamEnvironment:
     TODO: Should allow user to use their own queryable reward function (without
             needing to precalculate all the rewards)
     """
-    def __init__(self, spin_j):
-        self.grid_dimension = 5
+    def __init__(self, spin_j, spinfoam_model: BaseSpinFoam):
+        self.spinfoam_model = spinfoam_model
+        self.grid_dimension = self.spinfoam_model.n_boundary_intertwiners
         self.spin_j = float(spin_j)
         self.grid_length = int(2 * self.spin_j + 1)
-        vertex_amplitudes = tf.convert_to_tensor(
+        self.single_vertex_amplitudes = tf.convert_to_tensor(
             _load_vertex_amplitudes(self.spin_j), dtype=tf.float64
         )
-        norm = tf.math.sqrt(tf.math.reduce_sum(tf.math.square(vertex_amplitudes)))
-        self.rewards = tf.math.square(vertex_amplitudes / norm)
+        scale = tf.math.sqrt(
+            tf.math.reduce_sum(tf.math.square(self.single_vertex_amplitudes))
+        )
+        self.scaled_amplitudes = self.single_vertex_amplitudes / scale
 
     @tf.function
     def reset_for_forward_sampling(self, batch_size):
@@ -49,8 +104,19 @@ class SpinFoamEnvironment:
     @tf.function
     def get_rewards(self, positions):
         """Get the corresponding rewards for positions"""
-        rewards = tf.reshape(tf.gather_nd(self.rewards, positions), shape=(-1, 1))
+        rewards = tf.reshape(
+            self._get_squared_spinfoam_amplitudes(
+                self.scaled_amplitudes, positions
+            ),
+            shape=(-1, 1)
+        )
         return rewards
+
+    @tf.function
+    def _get_squared_spinfoam_amplitudes(self, amplitudes, positions):
+        return tf.math.square(
+            self.spinfoam_model.get_spinfoam_amplitudes(amplitudes, positions)
+        )
 
 
 def _load_vertex_amplitudes(spin_j):
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,4 +1,6 @@
-from core.environment import SpinFoamEnvironment, _load_vertex_amplitudes
+import tensorflow as tf
+
+from core.environment import SpinFoamEnvironment, SingleVertexSpinFoam, _load_vertex_amplitudes
 
 
 def test_loaded_amplitudes_have_correct_size():
@@ -21,16 +23,19 @@ def test_loaded_amplitude_values_are_correct():
     assert vertex_amplitude == expected_amplitude
 
 
-# def test_stored_squared_amplitudes_are_correct():
-#     spin_j = 3
-#     env = SpinFoamEnvironment(
-#         spin_j=spin_j
-#     )
-#
-#     i1, i2, i3, i4, i5 = 0, 3, 0, 2, 0
-#     # From Python_notebook.ipynb
-#     expected_amplitude = -5.071973704515683e-13
-#     expected_squared_amplitude = expected_amplitude**2
-#     stored_squared_amplitude = env.squared_amplitudes[i1, i2, i3, i4, i5]
-#
-#     assert stored_squared_amplitude == expected_squared_amplitude
+def test_squared_single_vertex_amplitudes_are_correct():
+    spin_j = 3
+    env = SpinFoamEnvironment(
+        spin_j=spin_j, spinfoam_model=SingleVertexSpinFoam()
+    )
+
+    i1, i2, i3, i4, i5 = 0, 3, 0, 2, 0
+    positions = tf.constant([i1, i2, i3, i4, i5])
+    # From Python_notebook.ipynb
+    expected_amplitude = -5.071973704515683e-13
+    expected_squared_amplitude = expected_amplitude**2
+    stored_squared_amplitude = env._get_squared_spinfoam_amplitudes(
+        env.single_vertex_amplitudes, positions
+    )
+
+    assert stored_squared_amplitude == expected_squared_amplitude
