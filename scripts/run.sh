b3090eee71188b39026894fe5046bee3132b9bd1
Jared Wogan <jared.wogan@gmail.com>
2023-05-09 17:13:27 -0400

    Script fix, created setup.py

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1        # request GPU "generic resource"
-#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M        # memory per node
 #SBATCH --time=0-08:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
@@ -12,6 +12,7 @@ module load python/3
 virtualenv --no-download tensorflow
 source tensorflow/bin/activate
 pip install --no-index tensorflow==2.8
+pip install -e ./../
 
 module load cuda cudnn 
 source tensorflow/bin/activate
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spinfoam_gflownets",
+    packages=find_packages(),
+)
