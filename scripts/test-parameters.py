8b9ce766c41f80bfce2efe1d43ff4e517bef4a11
Jared Wogan <jared.wogan@gmail.com>
2023-05-10 11:47:33 -0400

    Scripts fix

--- scripts/test-parameters.py
+++ scripts/test-parameters.py
@@ -23,27 +23,38 @@ class ModelParams:
     n_iterations: int
     evaluation_batch_size: int
     generate_samples_every_m_training_samples: int
+    _params_list: list = None
+    _idx: int = 0
+    
+    def __post_init__(self, *args, **kwargs):
+        self._params_list = list(itertools.product(*self.params()))
     
     def fields(self):
-        return list(self.__dataclass_fields__.keys())
-    
+        return list(filter(
+            lambda x: x.startswith("_") == False,
+            self.__dataclass_fields__.keys()
+        ))
+
     def params(self):
         return (getattr(self, field, ()) for field in self.fields())
     
     def __len__(self):
-        return len(list(itertools.product(*self.params())))
+        return len(self._params_list)
     
     def __iter__(self):
+        self._idx = 0
         return self
 
-    def __next__(self):        
-        for param in itertools.product(*self.params()):
-            return {
-                key: value
-                for key, value in zip(self.fields(), param)
-            }
-            
-        raise StopIteration 
+    def __next__(self):
+        if self._idx >= len(self):
+            raise StopIteration
+
+        p = {
+            key: value
+            for key, value in zip(self.fields(), self._params_list[self._idx])
+        }
+        self._idx += 1
+        return p
 
 single_model = ModelParams(
         sf_model = ["single_vertex_model"], # Input layer: 5 * (2 * spin  + 1), Output layer: forward = 5 + 1, backward = 5
@@ -95,17 +106,19 @@ tf.print("Starting testing:", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%
 for model in models:
     num_models = len(model)
     tf.print("Testing model:", model.sf_model[0], "\n")
+    with open(f"results/{model.sf_model[0]}-params.pkl", "wb") as f:
+        f.write("training_time, params, avg_losses\n")
     for i, params in enumerate(model):
         tf.print(f"Starting training for parameter set {i} of {num_models}")
         training_start = datetime.datetime.now()
-        avg_losses = train_gfn(**params)
+        avg_losses = train_gfn(**params, include_datetime_in_directory_name=False)
         training_time = (datetime.datetime.now() - training_start).total_seconds()
         tf.print("Finished training, elapsed time:", training_time / 60, "minutes")
         models_avg_losses[model.sf_model[0]].append((training_time, params, avg_losses))
 
+with open(f"results/{model.sf_model[0]}-params.pkl", "ab") as f:
+    pickle.dump(f, training_time, params, avg_losses)
+
 tf.print("Finished testing:", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 tf.print("Total time taken: ", (datetime.datetime.now() - start).total_seconds() / 60, "minutes")
 tf.print("Saving results")
-
-with open("ave_losses.pickle", "wb") as f:
-    pickle.dump(models_avg_losses, f)
